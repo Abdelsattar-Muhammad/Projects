cdf29f8284ddd73a1d3efbdb2fe8893603ea64d8
Abdelsattar Muhammad <130950623+abdelsatttar@users.noreply.github.com>
2023-05-04 15:55:37 +0300

    Add files via upload

---
+++ Connected 2 MCU Using SPI/MCU1/MCU1.c
@@ -0,0 +1,28 @@
+﻿/*
+ * MCU1.c
+ *
+ * Created: 02/05/2023 11:24:21 م
+ *  Author: Abdelsattar
+ */ 
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include <avr/io.h>
+#include "SPI.h"
+#include "KEYPAD.h"
+#include "data_types.h"
+int main(void)
+{
+	u8 val;
+	SPI_Init_Master();
+	Keypad_init();
+	 while(1)
+    {
+		do 
+		{
+			val=Keypad_Read();
+		} while (val==NOT_PRESSED);
+		SPI_Master_Send_Byte(val);
+		_delay_ms(250); //delay to avoid debouncing
+       
+    }
+}
---
+++ Connected 2 MCU Using SPI/MCU1/std_macro.h
@@ -0,0 +1,21 @@
+﻿/*
+ * std_macro.h
+ *
+ * Created: 12/04/2023 04:31:32 م
+ *  Author: Abdelsattar
+ */ 
+
+
+#ifndef STD_MACRO_H_
+#define STD_MACRO_H_
+
+
+#define REGISTER_SIZE 8
+#define SET_BIT(reg,bit) reg |= (1<<bit)
+#define CLR_BIT(reg,bit) reg &= (~(1<<bit))
+#define TOGGLE_BIT(reg,bit) reg ^= (1<<bit)
+#define READ_BIT(reg,bit) ((reg>>bit)&1)
+#define ROL(reg,num) reg=(reg<<num)|(reg>>(REGISTER_SIZE-num))
+#define ROR(reg,num) reg=(reg>>num)|(reg<<(REGISTER_SIZE-num))
+
+#endif /* STD_MACRO_H_ */
